381734902d3f43ffbeda340605af09bf8ef359a5
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2022-03-15 18:46:51 +0100

    Page 404 + redirect pour Netlify

---
+++ src/views/Error.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-card>
+    <h1 class="centerT">Erreur 404 : La page demandée n'existe pas</h1>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "Error",
+};
+</script>
